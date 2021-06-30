6da0d00d4b74bb76a4b3152bca89376cd9eec1bf
Clevershelf009 <matthewski490@yahoo.co.uk>
2021-06-30 01:31:08 +0100

    fixed type signature in WallFactory

--- src/WallFactory.cpp
+++ src/WallFactory.cpp
@@ -1,5 +1,5 @@
 #include "WallFactory.h"
 
-Wall WallFactory(float start_x, float start_y, float end_x, float end_y) {
-    return Wall(sf::Vector2f(start_x, start_y), sf::Vector2f(end_x, end_y));
+Wall *WallFactory(float start_x, float start_y, float end_x, float end_y) {
+    return new Wall(sf::Vector2f(start_x, start_y), sf::Vector2f(end_x, end_y));
 }
--- src/WallFactory.h
+++ src/WallFactory.h
@@ -1,3 +1,3 @@
 #include "Wall.h"
 
-Wall WallFactory(float start_x, float start_y, float end_x, float end_y);
+Wall *WallFactory(float start_x, float start_y, float end_x, float end_y);
